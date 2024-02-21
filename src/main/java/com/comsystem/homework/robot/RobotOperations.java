ee68f818a7e2f9a695f757ed7c0e985661fb1917
dimitrov727 <64378294+dimitrov727@users.noreply.github.com>
2024-02-21 19:28:44 +0200

    Creating the project and creating the first method

---
+++ src/main/java/com/comsystem/homework/robot/RobotOperations.java
@@ -0,0 +1,54 @@
+package com.comsystem.homework.robot;
+
+
+import com.comsystem.homework.model.RobotAction;
+import com.comsystem.homework.model.RobotPlan;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class RobotOperations {
+
+    /**
+     * An algorithm that converts a number of days into an action plan.
+     * @param days the number of days that the robot can work
+     * @return The action plan <em>must maximize</em> the number of stones that the robot will dig. In other words, this
+     *         algorithm must try to achieve the highest value of {@link RobotPlan#numberOfStones} possible for the
+     *         number of provided days. The value of {@link RobotPlan#numberOfDays} is equal to the input
+     *         days parameter
+     * @see RobotPlan
+     */
+    public RobotPlan excavateStonesForDays(int days) {
+        int stones = 0;
+        List<RobotAction> action = new ArrayList<>();
+        for (int i = 1; i <= days; i++) {
+            if (i % 2 != 0){
+                action.add(RobotAction.DIG);
+                stones++;
+            }else {
+                action.add(RobotAction.CLONE);
+            }
+        }
+
+        // TODO
+        return new RobotPlan(days,stones,action);
+    }
+
+    /**
+     * An algorithm that converts a number of stones into an action plan. Essentially this algorithm is the inverse of
+     * {@link #excavateStonesForDays(int)}.
+     * @param numberOfStones the number of stones the robot has to collect
+     * @return The action plan <em>must minimize</em> the number of days necessary for the robot to dig the
+     *         provided number of stones. In other words, this algorithm must try to achieve the lowest value of
+     *         {@link RobotPlan#numberOfDays} possible for the number of provided stones. The value of
+     *         {@link RobotPlan#numberOfStones} is equal to the numberOfStones parameter
+     * @see RobotPlan
+     */
+    public RobotPlan daysRequiredToCollectStones(int numberOfStones) {
+        // TODO
+        return null;
+    }
+
+}
